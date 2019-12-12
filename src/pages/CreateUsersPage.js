1e7356d88ad908c9639463242afb7c16c1b19cb8
Vladislav <bolilyivs@gmail.com>
2019-12-12 23:38:29 +0500

    fix user const

--- src/pages/CreateUsersPage.js
+++ src/pages/CreateUsersPage.js
@@ -6,8 +6,8 @@ import Query from '../config.js'
 import Cookies from 'universal-cookie';
 
 const type = [
-    { key: '0', text: 'User', value: '0' },
-    { key: '1', text: 'Admin', value: '1' }
+    { key: '0', text: 'Admin', value: '0' },
+    { key: '1', text: 'User', value: '1' }
   ]
 
 class CreateUsersPage extends React.Component{
